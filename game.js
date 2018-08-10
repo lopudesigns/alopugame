3f9c84bf31e5bd7a5312ca0e10b120d6560b85ad
lopu <ireply@myleisure.com.au>
2018-08-11 00:25:48 +1000

    ready

--- game.js
+++ game.js
@@ -6,7 +6,6 @@ prompt.start()
 
 function newgame() {
 	prompt.get("health", function (err, res) {
-
 		res.health = res.health || 5
 		var state = {
 			actions: [
@@ -27,77 +26,93 @@ function newgame() {
 				log: ''
 			},
 			player: Math.random() > .5 ? 0 : 1,
-			turn: 0
+			turn: 0,
+			log: ''
 		}
-		render(state)
-		nturn(state)
+		
+		renderloop(state)
+		nturn(state, state.player)
+		var playern = (2 / (state.player + 1)) - 1
+		nturn(state, playern)
 	})
 }
 
 
-function nturn(state) {
-	if (state.player == 0) {
+function nturn(state, playern) {
+	if(state.stop) {
+		return
+	}
+	if (playern == 0) {
 		prompt.get("action", function (err, res) {
 			state.action = res.action || state["0"].state
-			nnturn(state)
+			nnturn(state, playern)
 		})
 	} else {
 		state.action =
 			state.actions[Math.round(Math.random() * state.actions.length - 1)]
-		setTimeout(function () {
-			nnturn(state)
-		}, 300)
+			setTimeout(function () {
+				nnturn(state, playern)
+			}, Math.random()*2000)
 	}
+
 }
 
-function nnturn(state) {
+function nnturn(state, playern) {
 	var res = {
 		action: state.action
 	}
-	var player = state[state.player]
-	var oplayer = state[(2 / (state.player + 1)) - 1]
+	var player = state[playern]
+	var oplayer = state[(2 / (playern + 1)) - 1]
 	player.state = res.action ||
-		player.state
-	render(state)
-	state['0'].log = ''
-	state['1'].log = ''
-
-	if (res.action == 'a') {
-		if (oplayer.state !== 'd') {
-			oplayer.health--
-				oplayer.health -= oplayer.fear
-			oplayer.log = `Took ${oplayer.fear+1} damage`
-		} else {
-			player.fear += Math.random() > .95 ? Math.random() * 20 :
+	player.state
+		if (res.action == 'a') {
+			if (oplayer.state !== 'd') {
+				oplayer.health--
+					oplayer.health -= oplayer.fear
+					oplayer.log = `Took ${oplayer.fear+1} damage`
+			} else {
+				player.fear += Math.random() > .95 ? Math.random() * 20 :
+					Math.random() > 0.8 ? Math.random() * 10 : Math.random() * 2
+	
+			}
+		} else if (res.action == 'p') {
+			oplayer.fear += Math.random() > .95 ? Math.random() * 20 :
 				Math.random() > 0.8 ? Math.random() * 10 : Math.random() * 2
-
+		} else if (res.action == 'd') {
+			player.state = res.action
 		}
-	} else if (res.action == 'p') {
-		oplayer.fear += Math.random() > .95 ? Math.random() * 20 :
-			Math.random() > 0.8 ? Math.random() * 10 : Math.random() * 2
-	} else if (res.action == 'd') {
-		player.state = res.action
-	}
-	//    console.log(ani.dots)
-	state.turn++
-
-	render(state)
-
-
-
-	if (oplayer.health <= 0) {
-		console.log(`
-
-game over, player ${state.player} won
-
-`.padStart(15).rainbow)
-
-	} else {
-		state.player = (2 / (state.player + 1)) - 1
+		//    console.log(ani.dots)
+		state.turn++
+		// render(state)
+		setTimeout(function(){
+		}, 0)
+	
+	
 		setTimeout(function () {
-			nturn(state)
-		}, 300)
-	}
+			oplayer.log = ''
+			// state['1'].log = ''
+	
+		}, 1000)
+	
+		if (oplayer.health <= 0) {
+			state.log = `game over, player ${playern} won`.padStart(15).rainbow
+			state.stop = true
+			setTimeout(function(){
+				state['0'].log = ''
+				state['1'].log = ''
+				// setTimeout(function(){
+				// }, 200)
+				// render(state)
+				prompt.get("y", function (err, res) {
+					if(res.y == 'y'){
+						newgame()
+					}
+				})
+			}, 400)
+		} else {
+				nturn(state, playern)
+		}
+
 
 }
 
@@ -112,22 +127,75 @@ function restr(string, times) {
 
 function render(state) {
 	console.log(`
-    
+		
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
 
 ${restr('<3',state['1'].health).padStart(50).red.bold}
 
 ${'(*).(*)'.padStart(40).bold.yellow}
+${(state['1'].log ? 
+`_-------------------------------_
+Opponent ${state['1'].log}`.padStart(20) : `
 
 
+`)}
 ${'n__n'.padStart(15).rainbow.bold}
 
 ${restr('<3', 
 state['0'].health).padStart(15).red.bold}
    ${state['0'].state.padStart(25)}
-_-------------------------------
+_-------------------------------_
    ${(state['0'].log ? "You "+state['0'].log : 
-'')}${(state['1'].log ? "Opponent "+state['1'].log : '')}
+'')}${state.log}
 `)
 }
 
+function renderloop(state){
+	if(state.stop){
+		return
+	}
+	setTimeout(function(){
+		render(state)
+		renderloop(state)
+	}, 200)
+}
+
 newgame()
+
